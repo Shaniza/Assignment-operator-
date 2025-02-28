58ed405f4319e1ad0d9fdc959b9c63a83e0b477f
Shaniza <shaniza.rr17@gmail.com>
2025-02-28 23:25:39 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let x = 10
+console.log("Basic:", x)
+x += 5
+console.log("Add and assign:" , x)
+
+let y = 10
+console.log("Basic:", y)
+y -= 5
+console.log("Suntract and assign:" , y)
+
+let z = 10
+console.log("Basic:", z)
+z *= 5
+console.log("Multiply and assign:" , z)
+
+let p = 10
+console.log("Basic:", p)
+p /= 5
+console.log("divide and assign:" , p)
+
+let q = 12
+console.log("Basic:", q)
+q %= 5
+console.log("modulus and assign:" , q)
